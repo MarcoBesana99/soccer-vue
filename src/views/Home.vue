76b83d4cd6b007837bb4f3767fdd6ba9e7562486
Marco Besana <w61487@student.wsiz.rzeszow.pl>
2021-06-28 12:06:03 +0200

    fetch today matched before mount and create basic components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,43 @@
 <template>
+  <div>home</div>
   <div>
-    home
+    <div v-for="match in todayMatches" :key="match.id">
+      {{ match.competition.name }}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import NavBar from '@/components/NavBar.vue'
+import SideBar from '@/components/SideBar.vue'
+import MatchItem from '@/components/MatchItem.vue'
+import env from "@/env.js";
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  data () {
+    return {
+      todayMatches : []
+    }
+  },
+  components : {
+    NavBar, SideBar, MatchItem
+  },
+  beforeMount() {
+    fetch(`http://api.football-data.org/v2/matches`, {
+      method: "GET",
+      headers: {
+        "X-Auth-Token": `${env.apiKey}`,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        this.todayMatches = data.matches
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  },
+};
 </script>
